4dba62ed925455ece85c66988b9cc13e5c1ab653
Jeremy Antenberg <jantenberg0@gmail.com>
2016-10-19 19:34:12 +0000

    thugify

---
+++ thugify/script.js
@@ -0,0 +1,6 @@
+document.getElementById('go').onclick = function(){
+    var inputText = $('#text-input').val();
+    var thugifiedText = inputText + ", dawg";
+    document.getElementById('output').innerHTML = thugifiedText;
+    
+};
